a64d94c9aa26e75a6263f23a65e90b85e48b8d59
Bruno Renostro <66738966+brunorenostro@users.noreply.github.com>
2022-04-05 21:49:19 -0400

    day002

---
+++ day002/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int age = 10;
+	int height=180;
+	printf("I m %d years old.\n",age);
+	printf("I am %d cm tall.\n",height);
+	return 0;
+}
